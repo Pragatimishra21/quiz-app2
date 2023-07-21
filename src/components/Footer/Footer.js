df29c6c3d689c6dbefb9289d32c728c6c3783c66
Pragati <prags210702@gmail.com>
2023-07-21 15:49:31 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div
+         style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+       &copy; All Rights Reserved | Design By Pragati Mishra
+    </div>
+  )
+}
+
+export default Footer
